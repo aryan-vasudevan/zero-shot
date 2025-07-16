3bfd9c9fd65e1f3f8192d676e7f23836db3afad2
aryan-vasudevan <vasudevan.aryan@gmail.com>
2025-07-16 17:48:24 -0400

    Client and settings

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ from google.genai import types
 
 load_dotenv()
 
+client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
+
+safety_settings = [
+    types.SafetySetting(
+        category="HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold="BLOCK_ONLY_HIGH",
+    ),
+]
+
+MODEL_NAME = "gemini-2.5-flash-preview-05-20"
+TEMPERATURE = 0.5
+
+IMAGE_PATH = "/content/dog-3.jpeg"
+PROMPT = "Detect dogs tail. " + \
+"Output a JSON list of bounding boxes where each entry contains the 2D bounding box in the key \"box_2d\", " + \
+"and the text label in the key \"label\". Use descriptive labels."
