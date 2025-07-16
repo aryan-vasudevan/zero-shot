51e8f36af82c8d4b34d106549f6273a136445b8c
aryan-vasudevan <vasudevan.aryan@gmail.com>
2025-07-16 17:58:20 -0400

    API response

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from PIL import Image
 
 load_dotenv()
 
+# Load Gemini 2.5 and prompt
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 
 safety_settings = [
@@ -17,7 +19,27 @@ safety_settings = [
 MODEL_NAME = "gemini-2.5-flash-preview-05-20"
 TEMPERATURE = 0.5
 
-IMAGE_PATH = "/content/dog-3.jpeg"
-PROMPT = "Detect dogs tail. " + \
+IMAGE_PATH = "sample.jpg"
+PROMPT = "Detect the motorcycles." + \
 "Output a JSON list of bounding boxes where each entry contains the 2D bounding box in the key \"box_2d\", " + \
 "and the text label in the key \"label\". Use descriptive labels."
+
+# Image and response
+image = Image.open(IMAGE_PATH)
+width, height = image.size
+target_height = int(1024 * height / width)
+resized_image = image.resize((1024, target_height), Image.Resampling.LANCZOS)
+
+response = client.models.generate_content(
+    model=MODEL_NAME,
+    contents=[PROMPT, resized_image],
+    config = types.GenerateContentConfig(
+        temperature=TEMPERATURE,
+        safety_settings=safety_settings,
+        thinking_config=types.ThinkingConfig(
+          thinking_budget=0
+        )
+    )
+)
+
+print(response.text)
